0b756a6d7f08c5a8db39f52ee5aafa8b0bfc0d9d
rusroxana <rusroxana@EVO261.evointern>
2015-07-13 15:56:01 +0300

    Inbox Test, Constants

---
+++ QAProject/src/test/java/com/InboxTest.java
@@ -0,0 +1,39 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import tools.Constants;
+
+import com.steps.InboxSteps;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.pages.Pages;
+
+@RunWith(SerenityRunner.class)
+public class InboxTest {
+
+    @Managed(uniqueSession = true)
+    public WebDriver webdriver;
+
+    @ManagedPages(defaultUrl = Constants.EvoPortal)
+    public Pages pages;
+
+    @Steps
+    public InboxSteps inboxSteps;
+
+    @Test
+	public void checkVacationButton() {
+    	inboxSteps.openLoginPage();
+    	inboxSteps.enterUsername("Roxana");
+    	inboxSteps.enterPassword("test1");
+    	inboxSteps.startSearch();
+    	inboxSteps.startVacationbtn();
+    	inboxSteps.inboxbtn();
+    	inboxSteps.checkInboxButtonIsPressed();
+	}
+}
