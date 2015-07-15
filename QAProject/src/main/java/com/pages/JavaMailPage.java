3bffa3f7394e9593b8fddadbaf12df26accf5e3c
oanaceclan <oanaceclan@EVO458>
2015-07-15 15:28:11 +0300

    Java validate subject

--- QAProject/src/main/java/com/pages/JavaMailPage.java
+++ QAProject/src/main/java/com/pages/JavaMailPage.java
@@ -1,30 +1,57 @@
 package com.pages;
 
-import java.util.*;
-import javax.mail.*;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Store;
 
 public class JavaMailPage {
-    public static void main(String[] args) {
-        Properties props = new Properties();
-        props.setProperty("mail.store.protocol", "imaps");
-        try {
-            Session session = Session.getInstance(props, null);
-            Store store = session.getStore();
-            store.connect("mail.evozon.com", "oana.ceclan@evozon.com", "Oana.ceclan9");
-            Folder inbox = store.getFolder("INBOX");
-            inbox.open(Folder.READ_ONLY);
-            Message msg = inbox.getMessage(inbox.getMessageCount());
-            Address[] in = msg.getFrom();
-            for (Address address : in) {
-                System.out.println("FROM:" + address.toString());
-            }
-            //Multipart mp = (Multipart) msg.getContent();
-            //BodyPart bp = mp.getBodyPart(0);
-            System.out.println("SENT DATE:" + msg.getSentDate());
-            System.out.println("SUBJECT:" + msg.getSubject());
-            System.out.println("CONTENT:" + msg.getContent());
-        } catch (Exception mex) {
-            mex.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+
+		String Subject = "Vacation Request Cancellation";
+
+		Properties props = new Properties();
+		props.setProperty("mail.store.protocol", "imaps");
+
+		try {
+			Session session = Session.getInstance(props, null);
+			Store store = session.getStore();
+			store.connect("mail.evozon.com", "oana.ceclan@evozon.com", "Oana.ceclan9");
+			Folder inbox = store.getFolder("INBOX");
+			inbox.open(Folder.READ_ONLY);
+			Message msg = inbox.getMessage(inbox.getMessageCount());
+			Address[] in = msg.getFrom();
+			for (Address address : in) {
+				System.out.println("FROM:" + address.toString());
+			}
+			// Multipart mp = (Multipart) msg.getContent();
+			// BodyPart bp = mp.getBodyPart(0);
+			System.out.println("SENT DATE:" + msg.getSentDate());
+			System.out.println("SUBJECT:" + msg.getSubject());
+			System.out.println("CONTENT:" + msg.getContent());
+
+			String getSubject = msg.getSubject();
+
+			int result = Subject.compareTo(getSubject);
+			if (result == 0) {
+				System.out.println("ok");
+			} else {
+				System.out.println("not ok");
+			}
+
+
+		} catch (Exception mex) {
+			mex.printStackTrace();
+		}
+
+		// for (int i = 1; i < Message.length(); i++) {
+		// if(Message.get(i).getText().contains(Subject))
+		// System.out.println("Email validate");
+		//
+		// }
+
+	}
 }
